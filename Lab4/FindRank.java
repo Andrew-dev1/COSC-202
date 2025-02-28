c2b6961fd604b3cfe887a06d1b303d3ba7c131df
Andrew-dev1 <andrli20140@gmail.com>
2025-02-28 13:38:17 -0500

    was a good attempt, changing to while loop

--- Lab4/FindRank.java
+++ Lab4/FindRank.java
@@ -11,36 +11,102 @@ public class FindRank{
 
 
     public static <T extends Comparable<? super T>> T findRank(SortedList<T> list1, SortedList<T> list2, int rank) {
-        int smaller;
-        int larger;
-
-        if(list1.size() < list2.size()){
-            smaller = list1.size();
-            larger = list2.size();
-        } else{
-            smaller = list2.size();
-            larger = list1.size();
-        }
-
-        if(smaller +larger <= rank){
+        
+        // checks for ranks that are too big or too small
+        if(list1.size() +list2.size() <= rank || rank < 0){
             return null;
         }
-    
-        T object1 = list1.get(rank/2);
-        T object2 = list2.get(rank/2);
+        int size1 = list1.size();
+        int size2 = list2.size();
+        
+        T objectA ; 
+        T smallobjectB;
+        T bigobjectB;
+        // edge cases for empty lists
+        if(size1 == 0){ return list2.get(rank);}
+        if(size2 == 0){ return list1.get(rank);}
 
-        if(rank == 1){
-            if(object1.compareTo(object2) > 0){
-                return object2;
+        // ranks at the start and end of the lists
+        if(rank == 0){
+            objectA = list1.get(0);
+            smallobjectB = list2.get(0);
+            if(objectA.compareTo(smallobjectB) > 0){
+                return smallobjectB;
             }
             else{
-                return object1;
+                return objectA;
+            }
+        }
+        if(rank == size1+size2 -1){
+            objectA = list1.get(size1-1);
+            smallobjectB = list2.get(size2-1);
+            if(objectA.compareTo(smallobjectB) > 0){
+                return objectA;
+            }
+            else{
+                return smallobjectB;
             }
         }
 
+        if(rank >= size1 & list1.get(size1-1).compareTo(list2.get(0))< 0){
+           return list2.get(rank -size1);
+        }
+        else if(rank >= size2 & list2.get(size2-1).compareTo(list1.get(0))< 0){
+            return list1.get(rank -size1);
+        }
 
-        return null;    // placeholder
+        if (size1 > size2) {
+            return findRank(list2, list1, rank);
+        }
+
+
+
+        int middleA = rank/2;
+        int middleB = rank-middleA;
+        int lowBound = 0; 
+        int highBound = Math.min(rank, size1);
+
+        return findInner(list1, list2, rank, lowBound, highBound);
+
+        // return null;    // placeholder
         
+    }
+
+    public static <T extends Comparable<? super T>> T findInner(SortedList<T> list1, SortedList<T> list2, int rank, int low, int high){
+        int middleA = low + (low+high) /2;
+        int middleB = rank - middleA;
+
+        System.out.println("midA: " + String.valueOf(middleA)+ " midB: "  + String.valueOf(middleB));
+        if(middleB > list2.size()){
+            // if the index is greater than size of list2, middleA needs to be bigger
+            low = middleA +1; 
+        }
+        else if(middleB < 0){
+            // if the index is less than 0, middleA needs to be lower
+            high = middleA -1; 
+        }
+        else{
+            T objectA = list1.get(middleA);
+            T smallobjectB = list2.get(middleB);
+            T bigobjectB = list2.get(middleB+1);
+            
+            if(objectA.compareTo(smallobjectB) >0){
+                if(objectA.compareTo(bigobjectB )<0){
+                    return objectA;
+                }
+                else{
+
+                    if(smallobjectB.compareTo(list1.get(middleA+1) ) < 0){
+                        return smallobjectB;
+                    }
+                    return findInner(list1, list2, rank, middleA, middleB);
+                }
+
+            }else{
+                return findInner(list1, list2, rank, middleA, middleB);
+            }
+        }
+        return null;
     }
 
 
@@ -58,10 +124,12 @@ public class FindRank{
 
         System.out.println(list1);
         System.out.println(list2);
-        
-        System.out.println(findRank(list1, list2, 1));
+        System.out.println(findRank(list1, list2, 0));
+        System.out.println(findRank(list1, list2, 19));
+
         System.out.println(findRank(list1, list2, 3));
         System.out.println(findRank(list1, list2, 4));
+        System.out.println(findRank(list1, list2, 14));
         System.out.println(findRank(list1, list2, 20));
         System.out.println(findRank(list1, list2, 22));
 
