8c3f72f7184e9185d6344885f357a37b4a29cabb
Andrew-dev1 <andrli20140@gmail.com>
2025-04-17 11:38:56 -0400

    Completed the loop for calculating moves and repeating

--- Puzzle.java
+++ Puzzle.java
@@ -15,22 +15,22 @@ public class Puzzle {
     private static class boardState implements Comparable<boardState>  {
         int[][] board;
         private int heuristicvalue;
-        private int totalCost;
+        private int moves;
         String path = "";
 
-        public boardState(int[][] b, int heuristic, String _path, int moves){
+        public boardState(int[][] b, int heuristic, String _path, int move){
             board = b;
             heuristicvalue = heuristic;
             path = _path;
-            totalCost = moves +heuristic;
+            moves = move;
         }
 
         public int compareTo(boardState second){
-            int difference = this.totalCost - second.totalCost;
+            int difference = (this.heuristicvalue + moves)- (second.heuristicvalue +second.moves);
             return difference;
         }
-        public int cost(){
-            return totalCost;
+        public int move(){
+            return moves;
         }
 
         public String bToString(){
@@ -113,23 +113,33 @@ public class Puzzle {
 
         return puzzle;
     }
+
+    public static int[][] deepCopy(int[][] board){
+        int[][] newCopy = new int[board.length][board.length];
+        for(int i = 0; i< board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                newCopy[i][j] = board[i][j];
+            }
+        }
+        return newCopy;
+    }
     
-    // TODO: Implement this function
     public static String solvePuzzle(int[][] puzzle) {
         PriorityQueue<boardState> queue = new PriorityQueue<>();
         HashSet<String> visitedBoards = new HashSet<>();
 
         int h = heuristicCalculation(puzzle);
         queue.offer(new boardState(puzzle, h, "", 0));
-        System.out.println(h);
 
         while(! queue.isEmpty()){
             boardState currentBoard = queue.poll();
+
+            int[][] board = currentBoard.board;
             StringBuilder saveString = new StringBuilder(); 
             int x = -1; int y = -1;
-            for(int i = 0; i< puzzle.length; i++){
-                for(int j = 0; j < puzzle[0].length; j++){
-                    int number = puzzle[i][j];
+            for(int i = 0; i< board.length; i++){
+                for(int j = 0; j < board[0].length; j++){
+                    int number = board[i][j];
                     saveString.append(number);
                     if(number == 0){
                         x = i;
@@ -138,99 +148,46 @@ public class Puzzle {
                 }
             }
             String bString = saveString.toString();
-            // String bString = currentBoard.bToString();
 
-            // add board if not visited before 
-            if(!visitedBoards.contains(bString)){
-                visitedBoards.add(bString);
-            }
+            // add board if not visited before and skip board if seen 
+            if(visitedBoards.contains(bString))
+                continue; 
+            
+            visitedBoards.add(bString);
+            
 
             if(currentBoard.heuristicvalue == 0){
                 return currentBoard.path;
             }
 
-// Try moving in 4 directions
-int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
-char[] moves = {'U','D','L','R'};
+            // Try moving in 4 directions
+            // they move in terms of where the tiles slide 
+            int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
+            char[] moves = {'D','U','R','L'};
 
-for (int i = 0; i < 4; i++) {
-    int newX = x + directions[i][0];
-    int newY = y + directions[i][1];
+            for (int i = 0; i < 4; i++) {
+                int newX = x + directions[i][0];
+                int newY = y + directions[i][1];
 
-    if (newX >= 0 && New < puzzle.length && ny >= 0 && ny < puzzle[0].length) {
-        int[][] newBoard = deepCopy(currentBoard.board);
-        newBoard[x][y] = newBoard[nx][ny];
-        newBoard[nx][ny] = 0;
+                if (newX >= 0 && newX < puzzle.length && newY >= 0 && newY < puzzle[0].length) {
+                    int[][] newBoard = deepCopy(currentBoard.board);
+                    newBoard[x][y] = newBoard[newX][newY];
+                    newBoard[newX][newY] = 0;
 
-        String newPath = currentBoard.path + moves[i];
-        int newCost = heuristicCalculation(newBoard) + newPath.length(); // A* cost
+                    String newPath = currentBoard.path + moves[i];
+                    int newManhattan = heuristicCalculation(newBoard);
 
-        queue.offer(new boardState(newBoard, newCost, newPath));
-    }
-}
-
-        }
-
-
-
-        
-
-        return null;    // replace by returning a string corresponding to a sequence of moves to solve the puzzle
-    }
-
-    public static void recurse(int[][] puzzle, HashSet<String> visitedBoards, int n){
-
-        StringBuilder saveString = new StringBuilder(); 
-        int x = 0; int y = 0;
-        for(int i = 0; i< puzzle.length; i++){
-            for(int j = 0; j < puzzle[i].length; j++){
-                int number = puzzle[i][j];
-                saveString.append( Integer.toString(number));
-                if(number == 0){
-                    x = i;
-                    y = j;
+                    queue.offer(new boardState(newBoard, newManhattan, newPath, currentBoard.move()+1));
                 }
             }
-        }
-        if(!visitedBoards.contains(saveString.toString())){
-            visitedBoards.add(saveString.toString());
+
         }
 
-        // add to priority queue 
-
-        // move by 1, calculate heuristic value and call function again 
-        if((x+1) < n){
-            int swap = puzzle[x+1][y];
-            puzzle[x+1][y] = 0;
-            puzzle[x][y] = swap;
-            // call recursion 
-        }
-        if( x- 1 > 0){
-            int swap = puzzle[x+1][y];
-            puzzle[x+1][y] = 0;
-            puzzle[x][y] = swap;
-            // call recursion 
-        }
-        if((y+1) < n){
-            int swap = puzzle[x][y+1];
-            puzzle[x][y+1] = 0;
-            puzzle[x][y] = swap;
-            // call recursion 
-        }        
-        if((y-1) >0 ){
-            int swap = puzzle[x][y-1];
-            puzzle[x][y-1] = 0;
-            puzzle[x][y] = swap;
-            // call recursion 
-        }
-
-        // call next board in priority queue with lowest heuristic value 
-
+        return null;  // no solution found
     }
-    public boolean matched(){
 
-        return false;
-    }
+
+
 
     // done 
     public static int heuristicCalculation(int[][]puzzle){
@@ -280,26 +237,32 @@ for (int i = 0; i < 4; i++) {
         }
 
         String solution = solvePuzzle(puzzle);
-        System.out.println("Puzzle:");
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col++) {
-                System.out.printf("%4d", puzzle[row][col]);
-            }
-            System.out.println();
-        }
-        
-
         System.out.println("\nSolution:");
         System.out.println(solution);
 
-        int[][] puzzle1 = makePuzzle(size, 0);
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col++) {
-                System.out.printf("%4d", puzzle1[row][col]);
+        int[][] test = {{1,2,3},{7,4,5},{0,8,6}};
+        System.out.println("test:");
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.printf("%4d", test[row][col]);
             }
             System.out.println();
         }
-        System.out.println(Puzzle.heuristicCalculation(puzzle1));
+        solution = solvePuzzle(test);
+        System.out.println("\nSolution:");
+        System.out.println(solution);
+        
+
+        
+
+        // int[][] puzzle1 = makePuzzle(size, 0);
+        // for (int row = 0; row < size; row++) {
+        //     for (int col = 0; col < size; col++) {
+        //         System.out.printf("%4d", puzzle1[row][col]);
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println(Puzzle.heuristicCalculation(puzzle1));
 
     }
 }
